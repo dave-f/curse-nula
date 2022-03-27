5934b67562b65c079092aeaa281620321b7db448
dave-f <dave.footitt@gmail.com>
2022-03-27 11:27:42 +0100

    Start wiring up the assembler loader

--- main.asm
+++ main.asm
@@ -2,11 +2,42 @@
 ; 22 Feb 2022 -- 
 
 ORG &900
-.START:
-    RTS
-.END:
 
+.START:
+.LOAD_NULA:
+    LDX #LO(LOADER_NULA)
+    LDY #HI(LOADER_NULA)
+    JMP DO_LOAD
+
+.LOAD_NORMAL:
+    LDX #LO(LOADER_NORMAL)
+    LDY #HI(LOADER_NORMAL)
+
+.DO_LOAD:
+    JSR &FFF7
+    LDX #0
+
+.PROGRAM_PAL:
+    LDA PAL,X
+    STA &FE23
+    INX
+    CPX #32
+    BNE PROGRAM_PAL
+
+.START_GAME:
+    JMP &1900
+
+.PAL:
+    INCBIN "bin/game.pal"
+
+.LOADER_NULA:
+    EQUS "LOAD CURSE",13
+
+.LOADER_NORMAL:
+    EQUS "LOAD ORG",13
+
+.END:
     PUTFILE "bin/curse.new","CURSE",&1900,&1900
     PUTFILE "res/curse.bin","ORG",&1900,&1900
     PUTBASIC "loader.bas","Loader"
-    SAVE "dave",START,END
+    SAVE "PATCHER",START,END
